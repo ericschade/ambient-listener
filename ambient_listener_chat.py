4a2109a1d85ffd80c6c050e3b9e81cd924f2d803
Eric Schade <eric.schade@yahoo.com>
2024-02-17 13:03:44 -0800

    initial commit

---
+++ ambient_listener_chat.py
@@ -0,0 +1,96 @@
+import autogen
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+import chromadb
+import os
+import datetime
+
+config_list = autogen.config_list_from_json(
+        env_or_file="OAI_CONFIG_LIST",
+        file_location=".",
+        filter_dict={
+            "model": {
+                "gpt-4",
+            }
+        },
+    )
+
+llm_config = {
+    "timeout": 60,
+    "cache_seed": 42,
+    "config_list": config_list,
+    "temperature": 0,
+}
+
+def termination_msg(x):
+    return isinstance(x, dict) and "TERMINATE" == str(x.get("content", ""))[-9:].upper()
+
+# Make a list of the top level folders and file names in the reference_materials folder
+def get_reference_materials():
+    print("=========================================")
+    print("executing get_reference_materials.py from " + os.getcwd())
+    directory = "reference_materials/"
+    folders = []
+    files = []
+    for item in os.listdir(directory):
+        path = os.path.join(directory, item)
+        if os.path.isdir(path):
+            folders.append(directory + item)
+        else:
+            files.append(directory + item)
+    
+    return folders + files
+
+
+def main():
+    print("=========================================")
+
+    now = datetime.datetime.now()
+    current_date = now.strftime("%Y-%m-%d")
+    current_time = now.strftime("%H:%M:%S")
+
+    current_location = "the San Diego airport"
+
+
+    message = f"""
+    It is {current_time} on {current_date} and I am at {current_location}. I just heard an announcement over the loudspeaker saying the following:
+    Attention all passengers, the flight to New York City has been delayed by 2 hours. We apologize for the inconvenience. Please check the monitors for updates.
+    """
+
+    # 1. create RetrieveAssistantAgent instance named "assistant"
+    itinerary_retrieval_assistant = RetrieveAssistantAgent(
+        name="itinerary_retrieval_agent",
+        system_message="You are a helpful assistant.",
+        llm_config=llm_config,
+    )
+    
+    ragproxyagent = RetrieveUserProxyAgent(
+        name="ragproxyagent",
+        is_termination_msg=termination_msg,
+        human_input_mode="ALWAYS",
+        max_consecutive_auto_reply=10,
+        default_auto_reply="Reply `TERMINATE` if the task is done.",
+        retrieve_config={
+            "task": "code",
+            "docs_path": ["corpus/"],
+            "custom_text_types": ["json"],
+            "chunk_token_size": 2000,
+            "model": config_list[0]["model"],
+            "client": chromadb.PersistentClient(path="/tmp/chromadb"),
+            "embedding_model": "all-mpnet-base-v2",
+            "get_or_create": True,  # set to False if you don't want to reuse an existing collection, but you'll need to remove the collection manually
+        },
+        code_execution_config={
+                "work_dir": "autogen_code/",
+                "use_docker": False
+            },
+    )
+    assistant.reset()
+    ragproxyagent.reset()
+
+    # 3. initiate the chat
+    ragproxyagent.initiate_chat(assistant, clear_history=True, problem=message)
+
+
+if __name__ == "__main__":
+    main()
